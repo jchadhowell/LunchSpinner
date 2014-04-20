e300682acbcc33e01ff12b8d91fc991db74bba02
chad.howell <chad.howell@bancvue.com>
2014-04-19 23:09:40 -0500

    refactoring controller

---
+++ public/RestaurantsController.js
@@ -0,0 +1,19 @@
+restaurantApp.controller('RestaurantsController', function ($scope){
+    $scope.restaurants =  [
+        {name:'Whole Foods',type:'eclectic'},
+        {name:'Newks', type:'Sandwich'},
+        {name:'Chuys',type:'Tex-mex'},
+        {name:'Masala Wok', type:'Indian'},
+        {name:'Fire Bowl', type:'Asian'},
+        {name:'Panera',type:'sandwich'},
+        {name:'Noodles',type:'pasta'}
+    ];
+    $scope.doSort = function(propName){
+        $scope.sortBy = propName;
+        $scope.reverse = !$scope.reverse;
+    };
+
+    $scope.sortBy = 'name';
+    $scope.reverse = false;
+});
+
