ecd45329f671d5505143fb428cf602e1a0e9f603
chad.howell <chad.howell@bancvue.com>
2014-04-20 22:03:16 -0500

    refactoring controller

--- public/RestaurantsController.js
+++ public/RestaurantsController.js
@@ -1,19 +1,23 @@
-angular.module('restaurantApp').controller('RestaurantsController', function ($scope){
-    $scope.restaurants =  [
-        {name:'Whole Foods',type:'eclectic'},
-        {name:'Newks', type:'Sandwich'},
-        {name:'Chuys',type:'Tex-mex'},
-        {name:'Masala Wok', type:'Indian'},
-        {name:'Fire Bowl', type:'Asian'},
-        {name:'Panera',type:'sandwich'},
-        {name:'Noodles',type:'pasta'}
-    ];
-    $scope.doSort = function(propName){
-        $scope.sortBy = propName;
-        $scope.reverse = !$scope.reverse;
+(function(){
+    var RestaurantsController = function ($scope){
+        $scope.restaurants =  [
+            {name:'Whole Foods',type:'eclectic'},
+            {name:'Newks', type:'Sandwich'},
+            {name:'Chuys',type:'Tex-mex'},
+            {name:'Masala Wok', type:'Indian'},
+            {name:'Fire Bowl', type:'Asian'},
+            {name:'Panera',type:'sandwich'},
+            {name:'Noodles',type:'pasta'}
+        ];
+        $scope.doSort = function(propName){
+            $scope.sortBy = propName;
+            $scope.reverse = !$scope.reverse;
+        };
+
+        $scope.sortBy = 'name';
+        $scope.reverse = false;
     };
 
-    $scope.sortBy = 'name';
-    $scope.reverse = false;
-});
+    angular.module('restaurantApp').controller('RestaurantsController', RestaurantsController);
+})();
 
